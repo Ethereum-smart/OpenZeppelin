3dcb2219e7350749d8bf8c901cec22fc67722a9b
Kevin cuadros <kevindavidcuadros@gmail.com>
2022-05-17 21:18:32 -0500

    reto token fungible

---
+++ contracts/RetoTokenBurnablePausable.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TokenReto is Ownable, ERC20, ERC20Burnable, Pausable {
+
+  constructor() ERC20("Center Planet Core", "CPC") {
+    _mint(msg.sender, 10);
+  }
+
+  function miningToken () public onlyOwner { 
+      require(paused() == false, "Cannot mining more token");
+      _mint(msg.sender, 1);
+  }
+
+  function burnableToken ( uint32 amount ) public onlyOwner {
+      _burn(msg.sender, amount);
+  }
+
+  function pausableMining () public onlyOwner{
+    _pause();
+  }
+
+  function continueMining () public onlyOwner{
+    _unpause();
+  }
+
+}
